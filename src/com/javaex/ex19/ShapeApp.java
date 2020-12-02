8d8a5bacc7a325fb29f44892906df01d729a75f3
leenapark <hrn424@gmail.com>
2020-12-02 18:25:11 +0900

    20201202 배열로 관리 하기

---
+++ src/com/javaex/ex19/ShapeApp.java
@@ -0,0 +1,41 @@
+package com.javaex.ex19;
+
+public class ShapeApp {
+
+	public static void main(String[] args) {
+		//	메인
+		//	배열로 관리하기
+		Ractangle[] rArray = new Ractangle[3];
+		
+		
+		//	사각형 테스트
+		Ractangle r01 = new Ractangle("빨강", "노랑", 5,10);
+		Ractangle r02 = new Ractangle("검정", "하양", 11, 8);
+		Ractangle r03 = new Ractangle("분홍", "노랑", 11, 9);
+
+		//	배열에 주소값 대입
+		rArray[0] = r01;
+		rArray[1] = r02;
+		rArray[2] = r03;
+		
+		//	사각형 그리기
+		for(int i=0; i<rArray.length; i++) {
+			rArray[i].draw();
+		}
+		
+		
+		/*
+		//System.out.println(r01.toString()); //test
+		r01.draw();
+		r02.draw();
+		r03.draw();	
+		*/
+		
+		//원 테스트
+		Circle c01 = new Circle("파랑", "파랑", 5);
+		System.out.println(c01.toString());
+		c01.draw();
+		
+	}
+
+}
